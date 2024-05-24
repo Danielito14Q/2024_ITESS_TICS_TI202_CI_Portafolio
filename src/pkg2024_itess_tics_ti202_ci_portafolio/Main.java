227447f3206cb2100fb23430db1f6376f34be37a
Daniel Salas <danielsalasflores122@gmail.com>
2024-05-24 13:32:25 -0600

    Inicio del poryecto.

---
+++ src/pkg2024_itess_tics_ti202_ci_portafolio/Main.java
@@ -0,0 +1,20 @@
+/*
+ * ITESS - TICS
+ * Enero - Junio 2024
+ * Calculo Integral Portafolio
+ * Ti-202
+ * 24/05/2024
+ * Daniel Salas Flores
+ */
+package pkg2024_itess_tics_ti202_ci_portafolio;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
